960bedec53050b95aa04075244394ba3f2ed0b83
j.wrsm <100848785+wrsame@users.noreply.github.com>
2022-03-18 21:56:00 +0100

    idk

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static swot sw = new swot();
+
+    public static void main(String[] args) throws InterruptedException {
+        sw.introduction();
+        sw.userInput();
+
+
+
+
+        // Userinput
+        //ArrayList<String> strengths = new ArrayList<>();
+        //Scanner myInput = new Scanner(System.in);
+        //strengths.add(myInput.next());
+    }
+}
---
+++ src/com/company/swot.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class swot {
+  public void soutPrinter(String text) throws InterruptedException {
+
+    for (int i = 0; i <= text.length() - 1; i++) {
+      System.out.print(text.charAt(i));
+      Thread.sleep(20);
+    }
+    System.out.println();
+  }
+
+  public void introduction() throws InterruptedException {
+    // SWOT ANALYSIS Text
+
+    soutPrinter("SWOT ANALYSIS");
+    soutPrinter("A SWOT analysis is an incredibly simple, yet powerful tool to help you develop your business strategy, " +
+        "whether you’re building a startup or guiding an existing company.");
+    soutPrinter("SWOT stands for Strengths, Weaknesses, Opportunities, and Threats.");
+  }
+
+
+  public void userInput(){
+    Scanner in = new Scanner(System.in);
+   // Strengths
+    System.out.println("To do the analysis, you must answer the statements");
+    System.out.println("\n\nSTRENGTHS");
+    System.out.println("TYPE OUT 4 STRENGTH YOUR TEAM/COMPANY HAS");
+    String sth1 = in.nextLine();
+    String sth2 = in.nextLine();
+    String sth3 = in.nextLine();
+    String sth4 = in.nextLine();
+   //Weakness
+    System.out.println("WEAKNESS");
+    System.out.println("TYPE OUT 4 WEAKNESS YOUR TEAM/COMPANY HAS");
+    String wk1 = in.nextLine();
+    String wk2 = in.nextLine();
+    String wk3 = in.nextLine();
+    String swk4 = in.nextLine();
+   //Opportunities
+    System.out.println("OPPORTUNITIES");
+    System.out.println("TYPE OUT 4 OPPPORTUNTIES YOUR TEAM/COMPANY HAS");
+    String op1 = in.nextLine();
+    String op2 = in.nextLine();
+    String op3 = in.nextLine();
+    String op4 = in.nextLine();
+    //Threads
+    System.out.println("THREADS");
+    System.out.println("TYPE OUT 4 THREADS YOUR TEAM/COMPANY HAS");
+    String thr1 = in.nextLine();
+    String thr2 = in.nextLine();
+    String thr3 = in.nextLine();
+    String thr4 = in.nextLine();
+
+
+
+
+
+
+
+  }
+
+
+  //Print out the informations
+
+
+
+}
+
